58a2294c992f82352f5fb502d07090ef1c902ab1
Jitendrasinh Jadon <jituj6@gmail.com>
2020-11-11 15:41:04 +0530

    Adding files for first time

---
+++ src/main/java/com/covid/covidmanagement/CovidmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.covid.covidmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/covidmanagement/CovidmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.covidmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

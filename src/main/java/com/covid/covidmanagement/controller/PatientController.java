bd9835fa98cf9f51d9dfc7c3d6f677e7f4212e9e
Jitendrasinh Jadon <jituj6@gmail.com>
2020-11-15 14:09:19 +0530

    PatientController end points are added

--- src/main/java/com/covid/covidmanagement/controller/PatientController.java
+++ src/main/java/com/covid/covidmanagement/controller/PatientController.java
@@ -1,13 +1,18 @@
 package com.covid.covidmanagement.controller;
 
+import com.covid.covidmanagement.model.PatientDetailsDto;
 import com.covid.covidmanagement.service.PatientService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
@@ -17,22 +22,61 @@ public class PatientController {
 	PatientService patientService;
 
 
-	@PostMapping(path = "/covidCasesByCountryName")
-	public ResponseEntity<String> getCovidDetailsFromCountry()
-			throws JsonProcessingException {
-		Double discountedAmount = null;
+	@PostMapping(path = "/insertPatientDetails")
+	public ResponseEntity insertPatientDetails(@RequestBody PatientDetailsDto patientDetailsDto) {
 		HttpHeaders responseHeaders = new HttpHeaders();
+		Integer patientId = null;
 		responseHeaders.add("content-type", "application/json");
 		String errorMessage = "";
 		try {
-			} catch (Exception e) {
+			 patientId = patientService.savePatientDetails(patientDetailsDto);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			errorMessage = e.toString();
 		}
-		if (discountedAmount == null) {
+		if (!errorMessage.equalsIgnoreCase("")) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
 		} else {
-			return ResponseEntity.ok().headers(responseHeaders).body(discountedAmount.toString());
+			return ResponseEntity.ok().headers(responseHeaders).body(patientId);
+		}
+	}
+
+	@PostMapping(path = "/updatePatientDetails")
+	public ResponseEntity updatePatientDetails(@RequestBody PatientDetailsDto patientDetailsDto) {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		Integer patientId = null;
+		responseHeaders.add("content-type", "application/json");
+		String errorMessage = "";
+		try {
+			patientId = patientService.savePatientDetails(patientDetailsDto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			errorMessage = e.toString();
+		}
+		if (!errorMessage.equalsIgnoreCase("")) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+		} else {
+			return ResponseEntity.ok().headers(responseHeaders).body(patientId);
+		}
+	}
+
+
+	@GetMapping(path = "/allPatientDetails")
+	public ResponseEntity updatePatientDetails() {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		List<PatientDetailsDto> patientDetailsDtos = null;
+		responseHeaders.add("content-type", "application/json");
+		String errorMessage = "";
+		try {
+			patientDetailsDtos = patientService.getAllPatients();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			errorMessage = e.toString();
+		}
+		if (!errorMessage.equalsIgnoreCase("")) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+		} else {
+			return ResponseEntity.ok().headers(responseHeaders).body(patientDetailsDtos);
 		}
 	}
 
